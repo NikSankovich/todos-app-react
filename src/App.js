627c525f533d539ee75cc79ff32898bfe8a83bbe
NikSankovich <nik.sankovich@gmail.com>
2023-04-18 15:59:53 -0500

    updated app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isClicked: false,
+      todos: [],
+      text: ""
+    };
+
+  }
+  handleClick = () => {
+    //console.log("Clicked", this)
+    this.setState({
+      isClicked: !this.state.isClicked
+    })
+  }
+
+  handleChange = (event) => {
+    //console.log("Clicked", this)
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+
+  handleSubmit = () => {
+    this.setState({
+      todos: [...this.state.todos, this.state.text],
+      text: ""
+    })
+  }
+
+  delete = (index) => {
+    let objectCopy = [...this.state.todos]
+    objectCopy.splice(index, 1)
+    this.setState({ todos: [...objectCopy] })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Todo App</h1>
+        <input type="text" value={this.state.text} onChange={this.handleChange} />
+        <button onClick={this.handleSubmit}> Add Todo </button>
+
+        <h3>{this.state.isClicked === true ? "Clicked" : "Not Clicked"} </h3>
+        <button onClick={this.handleClick}> Click Me </button>
+        <ul>
+          {this.state.todos.map((todo, index) => {
+            return <li key={index}>
+              {todo}
+              <button onClick={() => this.delete(index)}>Delete</button>
+            </li>
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
+
+
+
+
